7cfdb305bb6053f3edb719f24f45983fdb4ccb93
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-08 13:00:13 +0000

    Sat, Oct 8, 2022, 1:00 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -5,11 +5,11 @@
 //prints hello, world
 int main(void)
 {
-    printf("hello , world\n");
+    printf("hello,world\n");
 }
 
 //obtain user's name
 string name = get_string("What's your name? ");
 
 //program greets with user's name
-/printf(hello, % s\n", name);
+printf(hello, % s\n", name);
