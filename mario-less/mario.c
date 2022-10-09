f47bfcfae5dcd8b523ccd465ed7ad8a3022d9a94
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-09 09:18:59 +0000

    Sun, Oct 9, 2022, 9:18 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -18,21 +18,28 @@ int main(void)
     printf(" \n");
 
 //to iterate rows
-for (int i=0;i < height;i++)
-        {
+    for (int i = 0; i < height; i++)
+    {
 
-        for (int j=0;j < height;j++)  //iterate columns
+        for (int j = 0; j < height; j++)  //iterate columns
         {
             //if-else loop to print number of blank char followed by #
-            if(i+j<height-1)
+            if (i + j < height - 1)
+
+            {
                 printf(" ");
+            }
+
             else
+
+            {
                 printf("#");
+            }
 
         }
 
         printf(" \n");  //move to next row after printing the colummns
-        }
-printf(" \n");  //empty space for styling purpose
+    }
+    printf(" \n");  //empty space for styling purpose
 
 }
