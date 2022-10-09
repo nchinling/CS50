39f58bbb14143e002e9e0ada6fca2f07b5d8e741
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-09 09:06:34 +0000

    Sun, Oct 9, 2022, 9:06 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -15,7 +15,7 @@ int main(void)
     while (height<1 || height>8);     //user will be reprompted for height
 
     //styling
-    printf(" \n");
+    //printf(" \n");
 
 //to iterate rows
 for (int i=0;i < height;i++)
