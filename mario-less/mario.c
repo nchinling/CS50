76f9232aa887c563c160b9151a1ee3e814a2f22a
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-09 07:33:38 +0000

    Sun, Oct 9, 2022, 7:33 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -11,7 +11,7 @@ int main(void)
         height=get_int("Positive Number: ");
 
 
-            printf("The height is %d",height);
+            printf("The height is %d"\n,height);
 
     }
     while (height<1 || height>8);     //user will be reprompted for height
