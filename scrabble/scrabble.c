b68818b9e65ee725343a365e02ca359e779afbc2
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-10 09:29:46 +0000

    Mon, Oct 10, 2022, 9:29 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -43,9 +43,18 @@ int main(void)
 
 int compute_score(string word)
 {
-    
-    score =
 
     // TODO: Compute and return score for string
+    int score = 0;
+    for (int i=0; i < word[], )
+
+
     return score
 }
+
+
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
