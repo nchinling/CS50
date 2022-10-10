e3cf1959ef946adb118e29a3f5e897a9104773e9
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-10 10:09:59 +0000

    Mon, Oct 10, 2022, 10:09 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,6 +5,7 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+//             {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q,  r, s, t, u, v, w, x, y, z}
 
 int compute_score(string word);
 
