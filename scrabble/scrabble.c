6483a620e8d49f64ddc3e35c6d92e89a4935e91e
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-10 09:10:00 +0000

    Mon, Oct 10, 2022, 9:10 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -11,12 +11,12 @@ int compute_score(string word);
 int main(void)
 {
     // Get input words from both players
-    string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
+    //string word1 = get_string("Player 1: ");
+    //string word2 = get_string("Player 2: ");
 
     // Score both words
-    int score1 = compute_score(word1);
-    int score2 = compute_score(word2);
+    int score1 = 20; //compute_score(word1);
+    int score2 = 8; //compute_score(word2);
 
     // TODO: Print the winner using if-elseif-else conditonal loop
     if  (score1 > score2)
