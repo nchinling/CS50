eb708dd20567e0b5f57f243a27d0329957d8badc
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-11 11:57:42 +0000

    Tue, Oct 11, 2022, 11:57 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,29 +8,28 @@ int count_letters(string text);
 // int count_words(string text);
 // int count_sentences(string text);
 
-
 int main(void)
 {
     int index;
 
-    string user_text = get_string("Enter text: \n");
+    string user_text = get_string("Enter text: ");
 
-    int L = count_letters(user_text);
+    //int L = count_letters(user_text);
     // int W = count_words(user_text);
     // int S = count_sentences(user_text);
 
 
-index = 0.0588 * 30 - 0.296 * 1 - 15.8;
-printf("%d",index);
+index = 0.0588 * 1 - 0.296 * 1 - 15.8;
+printf("%d\n",index);
 }
 
-int count_letters(string text)
-{
+// int count_letters(string text)
+// {
 
 
 
-     return int
-}
+//      return int
+// }
 
 // int count_words(string text)
 // {
