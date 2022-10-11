fadad72d5c234f27e7cba37409105fbb7124817e
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-11 11:54:58 +0000

    Tue, Oct 11, 2022, 11:54 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,44 +4,42 @@
 #include <string.h>
 
 //create function prototypes. Ends with semi-colon.
-int count_letters(string word);
-int count_words(string text);
-int count_sentences(string text);
-
+int count_letters(string text);
+// int count_words(string text);
+// int count_sentences(string text);
 
 
 int main(void)
 {
     int index;
 
-    string user_text = get_string("Enter text: ");
+    string user_text = get_string("Enter text: \n");
 
     int L = count_letters(user_text);
-    int W = count_words(user_text);
-    int S = count_strings(user_text);
+    // int W = count_words(user_text);
+    // int S = count_sentences(user_text);
 
 
 index = 0.0588 * 30 - 0.296 * 1 - 15.8;
-printf(round(index));
+printf("%d",index);
 }
 
-/* int count_letters(string text)
+int count_letters(string text)
 {
 
 
 
-return int
+     return int
 }
 
-int count_words(string text)
-{
+// int count_words(string text)
+// {
 
-return int
-}
+// return int
+// }
 
-int count_sentences(string text)
-{
+// int count_sentences(string text)
+// {
 
-return int
-}
- */
+// return int
+// }
