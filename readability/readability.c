5cdf8dea7b771b983c9f93069d24e6357097c26c
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-11 12:35:03 +0000

    Tue, Oct 11, 2022, 12:35 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -7,7 +7,7 @@
 //create function prototypes. Ends with semi-colon.
 int count_letters(string text);
 int count_words(string text);
-// int count_sentences(string text);
+int count_sentences(string text);
 
 int main(void)
 {
@@ -17,10 +17,12 @@ int main(void)
 
     int L = count_letters(user_text);
     printf("Letters:%d\n",L);
+
     int W = count_words(user_text);
     printf("Words:%d\n",W);
-    // int S = count_sentences(user_text);
 
+    int S = count_sentences(user_text);
+    printf("Sentences:%d\n",S);
 
 index = round(0.0588 * 1 - 0.296 * 1 - 15.8);
 printf("Grade:%d\n",index);
@@ -56,7 +58,7 @@ int count_letters(string text)
 }
 
 
-//Function to count number of words. Number of words is always 1 more than punctuations.
+//Function to count number of sentences. Number of sentences is always equal to number of punctuations.
 int count_sentences(string text)
 {
     int sentences;
@@ -83,22 +85,26 @@ int count_sentences(string text)
 
     }
 
-    sentences = punctuations + 1;
+    sentences = punctuations;
     return sentences;
 }
 
+
+//Function to count number of words. Number of words is always 1 more than number of spaces.
 int count_words(string text)
 {
-    int words = 0;
+    int words;
+    int spaces = 0;
+
 
     for (int i = 0; i < strlen(text); i++)
     {
 
-        if (text[i] == 33 || text[i] == 46 || text[i] == 63)
+        if (text[i] == 0)
 
         {
 
-            words++;
+            spaces++;
 
         }
 
@@ -106,16 +112,13 @@ int count_words(string text)
 
         {
 
-            words = words + 0;
+            spaces = spaces + 0;
 
         }
 
     }
 
-    sentences = punctuations + 1;
-    return sentences;
-
-
-
+    words = spaces + 1;
     return words;
+
 }
