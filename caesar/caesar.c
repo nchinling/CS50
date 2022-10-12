3daa8e36d0b8494b3ca18093bb0ad7e79db0c2ec
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-12 12:37:09 +0000

    Wed, Oct 12, 2022, 12:37 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,6 +6,20 @@
 
 int main(int argc, string argv[])
 {
+    // Make sure program was run with just one command-line argument
+
+    // Make sure every character in argv[1] is a digit
+
+    // Convert argv[1] from a `string` to an `int`
+
+    // Prompt user for plaintext
+
+    // For each character in the plaintext:
+
+        // Rotate the character if it's a letter
+
+
+
 //     for (int i = 0; i < argc; i++)
 //     {
 //         printf("%s\n", argv[i]);
