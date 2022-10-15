1a210ae814ee35c34896d772571783629e00f3ac
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-15 15:46:17 +0000

    Sat, Oct 15, 2022, 3:46 PM +00:00

---
+++ loop/loop2.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height; //declare variable height
+
+    //get height that mario needs to climb. Do-while lopp will execute at least once.
+    do
+    {
+        height = get_int("Height: ");
+
+
+    }
+    while (height < 1 || height > 8); //user will be reprompted for height
+
+
+
+//to iterate rows
+    for (int i = 0; i < height; i++)
+    {
+
+        for (int j = 0; i+j > height; j++)  //iterate columns
+        {
+            //if-else loop to print number of blank char followed by #
+            //if (i + j < height - 1)
+
+            //{
+                printf("#");
+            //}
+
+            //else
+
+            //{
+                //printf("#");
+            //}
+
+        }
+
+        printf("\n");  //move to next row after printing the colummns
+    }
+    //printf("\n");  //empty space for styling purpose
+
+}
