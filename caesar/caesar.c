871788317b054453d347ac9ae8d40c13abba3841
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-15 06:46:24 +0000

    Sat, Oct 15, 2022, 6:46 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,64 +4,93 @@
 #include <string.h>
 #include <stdlib.h>
 
+int only_digits(string numcheck);
+int rotate(char letter, int key);
+
 int main(int argc, string argv[])
 {
-    // Make sure program was run with just one command-line argument
-    // ensure argc ==2 using if-else loop. 
+    char code[1000];
 
-    // Make sure every character in argv[1] is a digit
+    // Make sure program was run with just one command-line argument by using argc == 2
+    if (argc == 2 && only_digits(argv[1]) == 0 )
+    {
 
-    // Convert argv[1] from a `string` to an `int`
+      // Convert argv[1] from a `string` to an `int`
+      int num = atoi(argv[1]);
+      //printf("The key is %d\n",num);
 
-    // Prompt user for plaintext
+      string plaintext = get_string("plaintext: ");
 
-    // For each character in the plaintext:
+        for (int i = 0; i < strlen(plaintext); i++)
+        {
 
-        // Rotate the character if it's a letter
+            char letter = plaintext[i];
 
+            code[i] = rotate(letter, num);
 
+        }
 
-//     for (int i = 0; i < argc; i++)
-//     {
-//         printf("%s\n", argv[i]);
-//     }
-  //string caesarc = cipher(argv[]);
+        printf("ciphertext:%s\n",code);
+    }
+
+    else
+    {
+
+      printf("Usage: %s key\n", argv[0]);
+
+    }
 
 }
 
-int only_digits(string argv[])
+
+// Make sure every character in argv[1] is a digit
+int only_digits(string numcheck)
 {
+    string digit = numcheck;
+    int val;
 
-//pseudocode
-//if string contains only 0-9 is false, return 1
-//else string contains 0-9 is true, return 0
+  for (int i = 0; i < strlen(digit); i++)
+  {
+      if (digit[i] - 48 >= 0 && digit[i] - 48 <= 9)
+      {
+        val = 0;
 
+      }
+      else
+      {
+        val = 1;
+      }
+
+  }
+
+    return val;
 }
 
-int rotate(char argv[])
+//Encryption function. For each character in the plaintext, rotate if it's a letter.
+int rotate(char letter, int key)
 {
+    char encipher;
+    char symbol;
 
+    if (isalpha(letter) && islower(letter))
+    {
+      encipher = ((letter - 97 + key) % 26) + 97;
+        //printf("%c", encipher);
+        return encipher;
+  }
 
+  else if (isalpha(letter) && isupper(letter))
+  {
+  encipher = ((letter - 65 + key) % 26) + 65;
+  //printf("%c", encipher);
+  return encipher;
+  }
+
+  else
+  {
+    symbol = letter;
+    //printf("%c", symbol);
+    return symbol;
+  }
 
 }
-
-
-// int main(int argc, string argv[])
-// {
-//     for (int i = 0; i < argc; i++)
-//     {
-//         printf("%s\n", argv[i]);
-//     }
-// }
-
-// int main(int argc, string argv[])
-// {
-//     for (int i = 0; i < argc; i++)
-//     {
-//         for (int j = 0, n = strlen(argv[i]); j < n; j++)
-//         {
-//             printf("%c\n", argv[i][j]);
-//         }
-//         printf("\n");
-//     }
-// }
