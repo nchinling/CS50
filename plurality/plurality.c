2592af4fdf5dbd2b97811fed81ce45fdadba2170
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-18 12:58:08 +0000

    Tue, Oct 18, 2022, 12:58 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -70,7 +70,7 @@ bool vote(string name)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(candidates[i].name, name) == 0)
+        if (strcmp(candidates.name, name) == 0)
         {
         candidates[i].votes=candidates[i].votes+1;
         //printf("%i\n", candidates[i].votes);
