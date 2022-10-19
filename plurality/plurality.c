1c7e54e63913e70b98800fb4556883b23a907e6d
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-19 13:12:08 +0000

    Wed, Oct 19, 2022, 1:12 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -84,7 +84,6 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    int max = 0;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes > max)
