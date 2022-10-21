60d886b725bc1394b31f873f7333e2ab86accc26
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-21 16:18:01 +0000

    Fri, Oct 21, 2022, 4:18 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,7 +127,7 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-        for (int i = 0; i < candidate_count; i++)
+        for (int i = 0; i < voter; i++)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
