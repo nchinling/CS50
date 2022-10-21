2e69df202266c1dca8adb6f8a37ebd325e9783a7
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-21 16:37:34 +0000

    Fri, Oct 21, 2022, 4:37 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -147,7 +147,7 @@ void tabulate(void)
     {
         if (candidates[i].eliminated == false)
         {
-            preferences[voter][rank] = i;
+            candidates[i];
             //return true;
         }
 
