42751722d5973f4090e0101ffc8722e7577c58bb
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-21 16:26:35 +0000

    Fri, Oct 21, 2022, 4:26 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -131,7 +132,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
-            preferences[voter][rank] = 
+            preferences[voter][rank] = 0;
             return true;
         }
 
