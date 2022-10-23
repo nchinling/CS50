136d2780fd26515953eb1166dc036a64be110ec6
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-23 07:49:32 +0000

    Sun, Oct 23, 2022, 7:49 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -41,9 +41,9 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    while(fread(&buffer, sizeof(int16_t), 1, input))
+    while (fread(&buffer, sizeof(int16_t), 1, input))
     {
-        buffer = buffer*factor;
+        buffer = buffer * factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
     }
 
