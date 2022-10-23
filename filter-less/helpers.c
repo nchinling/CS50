15c6f112e8094603ced820ee24243ff34e3a5574
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-23 16:26:56 +0000

    Sun, Oct 23, 2022, 4:26 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,18 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            RGBTRIPLE pix = image[h][w];
+            BYTE avg = round((pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed) / 3.0);
+            pixel.rgbtBlue = avg;
+            pixel.rgbtGreen = avg;
+            pixel.rgbtRed = avg;
+            image[h][w] = pixel;
+        }
+    }
     return;
 }
 
