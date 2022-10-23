340bbecdd2a07cfcf2159f3e736b6aefbfffa20c
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-23 07:05:30 +0000

    Sun, Oct 23, 2022, 7:05 AM +00:00

--- src4_memories/address1.c
+++ src4_memories/address1.c
@@ -1,11 +1,11 @@
-// Stores and prints a string without using the CS50 Library
+// Stores and prints a string's address via pointer arithmetic
 
 #include <stdio.h>
 
 int main(void)
 {
     char *s = "HI!";
-    printf("%c\n", s[0]);
-    printf("%c\n", s[1]);
-    printf("%c\n", s[2]);
+    printf("%c\n", *s);
+    printf("%c\n", *(s + 1));
+    printf("%c\n", *(s + 2));
 }
