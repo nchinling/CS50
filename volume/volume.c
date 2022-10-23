469ab0745647957d9880d953ccf548b64b14b7e1
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-23 07:30:26 +0000

    Sun, Oct 23, 2022, 7:30 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
-    fprintf(output, ")
+    
 
     // TODO: Read samples from input file and write updated data to output file
 
