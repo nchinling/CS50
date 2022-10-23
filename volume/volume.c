9a36ca4eafbc20b7539b86032f4b59cfecab5aea
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-23 04:54:39 +0000

    Sun, Oct 23, 2022, 4:54 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    fprintf
+    fprintf(output, ")
 
     // TODO: Read samples from input file and write updated data to output file
 
