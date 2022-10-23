2b6c9ea04815aa4f771c6282ca8430e88e1d5b4b
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-23 07:18:40 +0000

    Sun, Oct 23, 2022, 7:18 AM +00:00

--- src4_memories/address1.c
+++ src4_memories/address1.c
@@ -1,15 +1,25 @@
-// Prints two strings' addresses
+// Capitalizes a string
 
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    // Get two strings
-    char *s = get_string("s: ");
-    char *t = get_string("t: ");
+    // Get a string
+    string s = get_string("s: ");
 
-    // Print strings' addresses
-    printf("%p\n", s);
-    printf("%p\n", t);
+    // Copy string's address
+    string t = s;
+
+    // Capitalize first letter in string
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
+
+    // Print string twice
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
 }
