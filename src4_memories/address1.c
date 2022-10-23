f75e650136958cdc0d2a548da9d4c43b3779d589
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-23 07:14:36 +0000

    Sun, Oct 23, 2022, 7:14 AM +00:00

--- src4_memories/address1.c
+++ src4_memories/address1.c
@@ -1,8 +1,7 @@
-// Compares two strings using strcmp
+// Prints two strings
 
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
@@ -10,13 +9,8 @@ int main(void)
     char *s = get_string("s: ");
     char *t = get_string("t: ");
 
-    // Compare strings
-    if (strcmp(s, t) == 0)
-    {
-        printf("Same\n");
-    }
-    else
-    {
-        printf("Different\n");
-    }
+    // Print strings
+    printf("%s\n", s);
+    printf("%s\n", t);
 }
+
