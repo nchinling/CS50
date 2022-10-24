926f20a66186d8b5e1f269adaefce634d3fd35c7
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-24 03:04:44 +0000

    Mon, Oct 24, 2022, 3:04 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -70,11 +70,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             RGBTRIPLE temp = image[i][j];
 
             //most right pixel replaces most left pixel
-            image[i][j] = image[i][width-j];
+            image[i][j] = image[i][width-j+1];
 
             //temp (previously most left pix) replaces most right pixel
-            image[i][width-j] = temp;
-
+            image[i][width-j+1] = temp;
         }
     }
     return;
