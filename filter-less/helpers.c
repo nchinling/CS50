b3d499f463841877323b0fab4b5c5e0b1d54f0b2
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-24 02:27:19 +0000

    Mon, Oct 24, 2022, 2:27 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -23,7 +23,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        //For each column
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = 
 
+            //assign average to each rgbt
+            image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = average;
+        }
+    }
     return;
 }
 
