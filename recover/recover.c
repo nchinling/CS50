5580bf70a95eca6bc7bd8db7621543d247efc8de
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-24 04:37:35 +0000

    Mon, Oct 24, 2022, 4:37 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+ // Check command-line arguments. Only 1
+    if (argc != 2 || )
+    {
+        printf("Usage: ./recover filename.c\n");
+        return 1;
+    }
 }
