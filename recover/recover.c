f4cffdcf4fa9d72fbd04d0dac17f50db5da39bc4
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-26 15:18:38 +0000

    Wed, Oct 26, 2022, 3:18 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    while (fread(buffer, 1, BLOCK_SIZE, input) == BLOCK_SIZE)
+    while (fread(buffer, BLOCK_SIZE, 1, input) == 1)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
         {
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 
         if (output != NULL)
         {
-            fwrite(buffer, 1, BLOCK_SIZE, output);
+            fwrite(buffer, BLOCK_SIZE, 1, output);
         }
 
     }
