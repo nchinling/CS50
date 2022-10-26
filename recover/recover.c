f2021d22687f628a684ab89d029b19be0ee088d1
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-26 14:43:57 +0000

    Wed, Oct 26, 2022, 2:43 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
         {
-            sprintf(input, "%03i.jpg", 2);
+            sprintf(input, "%03i.jpg", count);
             FILE *img = fopen(input, "w");
             fwrite(input, BLOCK_SIZE, 1, output);
         }
