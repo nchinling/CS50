f57a5f838a9c26e2022e6fcc59ba835209acb6dd
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-27 12:35:14 +0000

    Thu, Oct 27, 2022, 12:35 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,11 +25,16 @@ int main(int argc, char *argv[])
     FILE *output = NULL;
     int count = 0;
     char filename[8];
-    
+
     while (fread(buffer, 1, BLOCK_SIZE, input) == BLOCK_SIZE)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
         {
+            if (count != NULL)
+            {
+                fclose(output);
+            }
+
             //create filename
             sprintf(filename, "%03i.jpg", count);
 
