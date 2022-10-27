77b5c64e2ad95fb5d088bd5db134557e022ca82b
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-27 12:26:35 +0000

    Thu, Oct 27, 2022, 12:26 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,11 +5,6 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    const int BLOCK_SIZE = 512;
-    BYTE buffer[BLOCK_SIZE];
-    FILE *output = NULL;
-    int count = 0;
-    char filename[8];
 
  // Check command-line arguments. Only 1
     if (argc != 2)
@@ -25,6 +20,11 @@ int main(int argc, char *argv[])
         return 2;
     }
 
+    const int BLOCK_SIZE = 512;
+    BYTE buffer[BLOCK_SIZE];
+    FILE *output = NULL;
+    int count = 0;
+    char filename[8];
     while (fread(buffer, 1, BLOCK_SIZE, input) == BLOCK_SIZE)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
 
     }
 
-    
+
     fclose(output);
     fclose(input);
 
