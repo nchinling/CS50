df2d8046bdc54ef08dc4056c6633feb55df8ba86
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-29 14:12:57 +0000

    Sat, Oct 29, 2022, 2:12 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -67,14 +67,13 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-        p->parents[0] = NULL;
-        p->parents[1] = NULL;
+        p -> parents[0] = NULL;
+        p -> parents[1] = NULL;
 
 
         // TODO: Randomly assign alleles
-        p->alleles[0] = random_allele();
-        p->alleles[1] = random_allele();
-
+        p -> alleles[0] = random_allele();
+        p -> alleles[1] = random_allele();
 
     }
 
