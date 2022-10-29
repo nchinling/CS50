53dcd522afa3ee2c90a00588d0cf3dac54c6386f
nchinling <109836399+nchinling@users.noreply.github.com>
2022-10-29 14:08:41 +0000

    Sat, Oct 29, 2022, 2:08 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -86,8 +86,14 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
+    if (p == NULL)
+    {
+        return;
+    }
 
     // TODO: Free parents recursively
+    free_family(p -> parents[0]);
+    free_family(p -> parents[1]);
 
     // TODO: Free child
         if (p == NULL)
