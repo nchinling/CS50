ec0691e4a3416aaa0f32e3aaa91280d8231c8825
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 02:43:29 +0000

    Sat, Nov 5, 2022, 2:43 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -22,17 +22,6 @@ def main():
         reader = csv.DictReader(file)
         print(f"{reader}")
 
-        people = {
-    "Carter": "+1-617-495-1000",
-    "David": "+1-949-468-2750"
-}
-
-# Search for name
-name = get_string("Name: ")
-if name in people:
-    print(f"Number: {people[name]}")
-
-
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
 
