d99b25d8cb791c2e3597307d0e80000ed0437cf3
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 02:47:56 +0000

    Sat, Nov 5, 2022, 2:47 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -18,7 +18,7 @@ def main():
     teams = []
 
     # TODO: Read teams into memory from file
-    with open(argv[2]) as file:
+    with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
         print(f"{reader}")
 
@@ -41,9 +41,9 @@ def simulate_game(team1, team2):
 def simulate_round(teams):
     """Simulate a round. Return a list of winning teams."""
     winners = []
-    for i in range(15):
-    winner = simulate_game()
-    winners.append(winner)
+    #for i in range(15):
+    #winner = simulate_game()
+    #winners.append(winner)
 
     # Simulate games for all pairs of teams
     for i in range(0, len(teams), 2):
