76ee587fe9dfdfc069ef3ce32e9b276a565a32db
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 12:29:57 +0000

    Sat, Nov 5, 2022, 12:29 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -24,7 +24,7 @@ def main():
         print(dnaseq)
 
     # TODO: Find longest match of each STR in DNA sequence
-        print(database[0])
+        longest_match(dnaqseq, )
 
     # TODO: Check database for matching profiles
 
