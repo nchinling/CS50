cc50a3d0a32234b7ff52797ab228d7885c7d0bde
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 09:54:42 +0000

    Sat, Nov 5, 2022, 9:54 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -14,8 +14,8 @@ def main():
     with open(sys.argv[2]) as datafile:
         read_database = csv.DictReader(datafile)
         for data in read_database:
-            data["name"] = int(data["name"])
-            database.append(data)
+            print(data['name'])
+            database.append(data[1])
         print(f"{database}")
 
     # TODO: Read DNA sequence file into a variable
