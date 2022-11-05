e58447439158586dc2fe5b99d1264147842976b3
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 06:17:27 +0000

    Sat, Nov 5, 2022, 6:17 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,12 +5,10 @@ def main():
     amount = get_amount()
 
     print(f"Amount is {amount}")
+    print(f"Quarters: {calculate_quarters(amount)}")
 
 
-
-
-
-# function to get height
+# function to get amount from user
 
 def get_amount():
     while True:
@@ -26,12 +24,28 @@ def get_amount():
                 return int(100*n)
 
 
+def calculate_quarters(amount):
+
+    return int((amount) / 25)
+
+def calculate_dimes(amount):
+
+    return int((amount) / 10)
+
+def calculate_nickel(amount):
+
+    return int((amount) / 5)
+
+def calculate_penny(amount):
+
+    return int((amount) / 1)
+
 main()
 
 
 
 
-int calculate_quarters(int cents);
-int calculate_dimes(int cents);
-int calculate_nickels(int cents);
-int calculate_pennies(int cents);
+# int calculate_quarters(int cents);
+# int calculate_dimes(int cents);
+# int calculate_nickels(int cents);
+# int calculate_pennies(int cents);
