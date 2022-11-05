8fd7c806fcb59cdbf4414295cac5f6a57c01dc67
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 07:30:33 +0000

    Sat, Nov 5, 2022, 7:30 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,22 @@
 # TODO
+
+def main():
+    # get text from user
+    text = get_text()
+
+    print(f"Text is: {text}")
+
+# function to get text from user
+def get_text():
+    while True:
+        try:
+            s = input("Provide text: ")
+        except ValueError:
+            print("That is not a text!")
+
+        else:
+            return s
+
+
+# run main function
+main()
