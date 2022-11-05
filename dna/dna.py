2beba61f36c2db03d046ad36a9f378451c8b570c
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 14:08:44 +0000

    Sat, Nov 5, 2022, 2:08 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -66,8 +66,6 @@ def main():
         if str_freq == int_person_val:
             print(f"Person is {person[0]}")
             break
-        else:
-            print("No match")
         i += 1
 
 
