73a53a07890671a19423adaf5c3d50542b3b62bc
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 04:51:56 +0000

    Sat, Nov 5, 2022, 4:51 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,16 +1,21 @@
 # TODO
 
 def main():
-    #
+    # get height from user
     height = get_height()
 
+    # print number of rows
     for i in range(height):
+        # print number of blank spaces
         for j in range(height - (i+1)):
             print(" ", end="")
+        # print #
         for k in range(i+1):
             print("#", end="")
 
         print()
+
+# function to get height
 
 
 def get_height():
