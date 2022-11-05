9847314c1c177dd2e23d8e876ac2b143ca1e0a35
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 07:36:21 +0000

    Sat, Nov 5, 2022, 7:36 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -6,6 +6,7 @@ def main():
 
     print(f"Text is: {text}")
 
+
 # function to get text from user
 def get_text():
     while True:
@@ -16,6 +17,13 @@ def get_text():
 
         else:
             return s
+
+# function to calculate index
+def cal_index():
+    # get text from user
+    text = get_text()
+
+    print(f"Text is: {text}")
 
 # run main function
 main()
