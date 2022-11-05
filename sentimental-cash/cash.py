57aa8b4516186cbf858570da3f36395599592a52
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 05:00:39 +0000

    Sat, Nov 5, 2022, 5:00 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,1 +1,33 @@
 # TODO
+
+def main():
+    # get height from user
+    height = get_height()
+
+    # print number of rows
+    for i in range(height):
+        # print number of blank spaces
+        for j in range(height - (i+1)):
+            print(" ", end="")
+        # print #
+        for k in range(i+1):
+            print("#", end="")
+
+        print()
+
+# function to get height
+
+
+def get_height():
+    while True:
+        try:
+            n = int(input("Height: "))
+        except ValueError:
+            print("That's not an integer!")
+        else:
+            if n > 0 and n < 9:
+                return n
+
+
+main()
+
