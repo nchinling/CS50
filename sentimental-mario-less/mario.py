94be7de4fa3ced51001cc8920798851e64121ba4
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 03:45:34 +0000

    Sat, Nov 5, 2022, 3:45 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,6 +1,22 @@
 # TODO
 
-for i in range(3):
-    for j in range(3):
-        print("#", end="")
-    print()
+def main():
+    height = get_height()
+    for i in range(height):
+        for j in range(height):
+            print("#", end="")
+        print()
+
+
+def get_height():
+    while True:
+        try:
+            n = int(input("Height: "))
+        except ValueError:
+            print("That's not an integer!")
+        else:
+            if n > 0:
+                return n
+
+
+main()
