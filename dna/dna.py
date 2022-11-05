f175daaa75456e03f05d6aec94287bab12051c7c
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 13:21:00 +0000

    Sat, Nov 5, 2022, 1:21 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -29,11 +29,11 @@ def main():
     subsequence = database[0].keys()
 
     # form change dictionary key values to a list
-    subsequence_list = list(subsequence)
+    subsequence_list = list(subsequence[1:])
 
     # for debugging
     print(subsequence_list)
-    print(len(subsequence_list))
+    print(len(subsequence_list[1:]))
 
     count = 0
     while (count < len(subsequence_list)):
