c28ed8b239f87c91f7ecfb43085371a0a3295b4d
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 12:27:12 +0000

    Sat, Nov 5, 2022, 12:27 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -6,7 +6,7 @@ def main():
 
     # TODO: Check for command-line usage
     if len(sys.argv) != 3:
-        print("Usage: python dna.py data.csv sequence.txt")
+        sys.exit("Usage: python dna.py data.csv sequence.txt")
 
     # TODO: Read database file into a variable
     database = []
