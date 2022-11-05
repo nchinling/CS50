ab5e5fcfcc996e8f4c6b23fe87cce9d4ff5e26fe
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 04:29:48 +0000

    Sat, Nov 5, 2022, 4:29 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -5,6 +5,8 @@ def main():
     for i in range(height):
         for j in range(height - (i+1)):
             print("#", end = "")
+            for k in range(i+1):
+                print("a")
 
         print()
 
