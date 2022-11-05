41e9c070721daea3954a6af6a7e879fc2a0e1d47
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-05 10:10:55 +0000

    Sat, Nov 5, 2022, 10:10 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -21,8 +21,8 @@ def main():
     # TODO: Read DNA sequence file into a variable
     dnaseq = []
 
-    dnafile = open(sys.argv[2], 'r')
-    print(dnafile)
+    dnaseq = open(sys.argv[2], 'r')
+    print(dnaseq)
 
     # TODO: Find longest match of each STR in DNA sequence
 
