6fbec212baeeca39cf3e6477ce961929f16f8e3e
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-09 13:59:45 +0000

    Wed, Nov 9, 2022, 1:59 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -2,3 +2,7 @@
 -- Your query should output a table with a single column for the name of each person.
 -- You may assume that there is only one movie in the database with the title Toy Story.
 
+-- SELECT .title, ratings.rating FROM movies
+-- JOIN ratings ON movies.id = ratings.movie_id
+-- WHERE year = 2010
+-- ORDER BY rating DESC, title
