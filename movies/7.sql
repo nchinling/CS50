b6bf3a9a47c0c1b5a53cfae1aed1ff7c18cd4ebf
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-09 13:37:25 +0000

    Wed, Nov 9, 2022, 1:37 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,6 +1,6 @@
--- SELECT title FROM movies WHERE year = 2010;
--- JOIN ratings ON movies.id = ratings.movie_id
--- SELECT ratings.rating;
+SELECT Column_Fname, Column_Lname, table_Department.Column_Dname
+FROM Table_Employee
+WHERE Table_Employee.Column_DeptNo = table_Department.Column_DeptNo
 
 
 SELECT movies.title, ratings.rating
