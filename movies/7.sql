adbbd08c64d467ba6081520adc4d5e2a8adb9eed
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-09 13:34:07 +0000

    Wed, Nov 9, 2022, 1:34 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,6 +1,10 @@
-SELECT title FROM movies WHERE year = 2010;
-JOIN ratings ON movies.id = ratings.movie_id
-SELECT ratings.rating;
+-- SELECT title FROM movies WHERE year = 2010;
+-- JOIN ratings ON movies.id = ratings.movie_id
+-- SELECT ratings.rating;
 
+
+SELECT t1.title, t2.rating
+FROM movies t1, ratings t2
+WHERE t1.year = 2010
 
 
