9ad953466c9424957428a0ee37489da77b3ab2fc
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-09 13:43:53 +0000

    Wed, Nov 9, 2022, 1:43 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,12 +1,17 @@
-SELECT Column_Fname, Column_Lname, table_Department.Column_Dname
-FROM Table_Employee
-WHERE Table_Employee.Column_DeptNo = table_Department.Column_DeptNo
+-- SELECT Column_Fname, Column_Lname, table_Department.Column_Dname
+-- FROM Table_Employee
+-- WHERE Table_Employee.Column_DeptNo = table_Department.Column_DeptNo
 
 
-SELECT movies.title, ratings.rating
-FROM movies, ratings
-WHERE movies.year = 2010;
+-- SELECT movies.title, ratings.rating
+-- FROM movies, ratings
+-- WHERE movies.year = 2010;
 
-In 7.sql, write a SQL query to list all movies released in 2010 and their ratings, in descending order by rating. For movies with the same rating, order them alphabetically by title.
-Your query should output a table with two columns, one for the title of each movie and one for the rating of each movie.
-Movies that do not have ratings should not be included in the result.
+SELECT title, rating FROM movies, ratings
+WHERE movies.id = ratings.movie_id and movies.year = 2010
+ORDER rating  DESC
+
+
+-- In 7.sql, write a SQL query to list all movies released in 2010 and their ratings, in descending order by rating. For movies with the same rating, order them alphabetically by title.
+-- Your query should output a table with two columns, one for the title of each movie and one for the rating of each movie.
+-- Movies that do not have ratings should not be included in the result.
