1e303d0cdab651f3a76f08693a49f21587d0bc85
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-10 11:45:11 +0000

    Thu, Nov 10, 2022, 11:45 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,7 +2,7 @@
 --1. To start running database queries
 sqlite3 fiftyville.db
 
---query the different types of tables available
+--2. Query the different types of tables available
 .tables
 airports              crime_scene_reports   people
 atm_transactions      flights               phone_calls
