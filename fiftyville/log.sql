3f790725e6592707471ecb074329d1df8a14e70c
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-10 12:35:23 +0000

    Thu, Nov 10, 2022, 12:35 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -30,4 +30,6 @@ AND month = 7 AND year = 2021;
 --Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
 
 --5.
-SELECT transcript 
+SELECT transcript FROM interviews
+WHERE year = 2021 AND
+month = 7 AND day = 28; 
