99f5b1d903bf699c110c77f585926e95ca01b41c
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-10 11:52:16 +0000

    Thu, Nov 10, 2022, 11:52 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,4 +9,15 @@ atm_transactions      flights               phone_calls
 bakery_security_logs  interviews
 bank_accounts         passengers
 
+--3. Query to know the columns in each table
+.schema airports
+.schema atm_transactions
+.schema bakery_security_logs
+.schema bank_accounts
+.schema crime_scene_reports
+.schema flights
+.schema interviews
+.schema passengers
+.schema people
+.schema phone_calls
 
