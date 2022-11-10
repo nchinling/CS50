5769fdaf002713b1860e083a1de5c2851b92dc98
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-10 11:41:10 +0000

    Thu, Nov 10, 2022, 11:41 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,5 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
+--query the different types of tables available 
 .tables
 airports              crime_scene_reports   people
 atm_transactions      flights               phone_calls
