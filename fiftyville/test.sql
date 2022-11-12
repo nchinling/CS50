bb2c6ce200385c5648bd8af4d95dd7d5f23eccd6
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-12 11:52:16 +0000

    Sat, Nov 12, 2022, 11:52 AM +00:00

--- fiftyville/test.sql
+++ fiftyville/test.sql
@@ -18,10 +18,11 @@ AND phone_calls.duration < 60 AND people.name IN (
         JOIN flights on flights.id = passengers.flight_id
         WHERE flights.year = 2021 AND
         flights.month = 7 AND
-        flights.day = 29 AND
-        MIN(flights.hour)
+        flights.day = 29
         )
 );
+
+sqlite3 fiftyville.db
 
     -- SELECT name FROM people
     --     JOIN passengers on passengers.passport_number = people.passport_number
