406beb3b775d9e75a26166436cda5a3ecf1cfa98
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-12 13:53:51 +0000

    Sat, Nov 12, 2022, 1:53 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -199,7 +199,7 @@ AND phone_calls.duration < 60 AND people.name IN (
         WHERE flights.year = 2021 AND
         flights.month = 7 AND
         flights.day = 28 AND
-        flights.hour < 9 
+        flights.hour < 9
         )
 );
 
@@ -246,7 +246,6 @@ AND phone_calls.duration < 60 AND people.name IN (
             hour = 10 AND
             minute BETWEEN 15 AND 25 AND
             activity = "exit"
-
             )
         )
 );
