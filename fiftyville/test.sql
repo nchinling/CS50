e26600c259ceb7f6966d29d9e471252edbe9cd85
nchinling <109836399+nchinling@users.noreply.github.com>
2022-11-12 11:43:18 +0000

    Sat, Nov 12, 2022, 11:43 AM +00:00

--- fiftyville/test.sql
+++ fiftyville/test.sql
@@ -18,7 +18,8 @@ AND phone_calls.duration < 60 AND people.name IN (
         JOIN flights on flights.id = passengers.flight_id
         WHERE flights.year = 2021 AND
         flights.month = 7 AND
-        flights.day = 29
+        flights.day = 29 AND
+        flights.hour < 7
         )
 );
 
