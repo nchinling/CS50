06273d4395d27f3a9e84e68126e15519ee90f40b
nchinling <109836399+nchinling@users.noreply.github.com>
2022-12-04 08:53:58 +0000

    Sun, Dec 4, 2022, 8:53 AM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -34,6 +34,10 @@ def index():
 
         # TODO: Display the entries in the database on index.html
 
+@app.route("/registrants")
+def registrants():
+    registrants = db.execute("SELECT * FROM registrants")
+    return render_template("registrants.html", registrants=registrants)
         return render_template("index.html")
 
 
